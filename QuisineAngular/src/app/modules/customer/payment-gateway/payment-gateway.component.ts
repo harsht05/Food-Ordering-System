72fe7e76dda300726204962aaa1516e98cff4d32
harshtr <harshtr@cybage.com>
2024-03-19 15:15:12 +0530

    intial commit in Angular

---
+++ QuisineAngular/src/app/modules/customer/payment-gateway/payment-gateway.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-gateway',
+  templateUrl: './payment-gateway.component.html',
+  styleUrl: './payment-gateway.component.css'
+})
+export class PaymentGatewayComponent {
+
+}
