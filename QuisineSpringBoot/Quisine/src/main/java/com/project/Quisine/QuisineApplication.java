9e4716a50d99d53e8e39cd05f803b9e9314246a1
harshtr <harshtr@cybage.com>
2024-03-19 11:08:57 +0530

    Project Initialization

---
+++ QuisineSpringBoot/Quisine/src/main/java/com/project/Quisine/QuisineApplication.java
@@ -0,0 +1,13 @@
+package com.project.Quisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuisineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuisineApplication.class, args);
+	}
+
+}
---
+++ QuisineSpringBoot/Quisine/src/test/java/com/project/Quisine/QuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Quisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuisineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
