03405e7d6e03f3f350aeb28cd86697276f6dc29e
Janhavi <janhavis@cybage.com>
2024-03-20 18:54:16 +0530

    Login & Otp done

--- QuisineSpringBoot/Quisine/src/main/java/com/project/Quisine/entity/UserEntity.java
+++ QuisineSpringBoot/Quisine/src/main/java/com/project/Quisine/entity/UserEntity.java
@@ -1,5 +1,37 @@
 package com.project.Quisine.entity;
 
-public class UserEntity {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	
+	private String userName;
+	private String userEmail;
+	private String userPass;
+	private String userContact;
+	private String userCity;
+	private String userState;
+	private int userPin;
+	private String role;
+	
+	@OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RestaurantFood> restaurantFoods;
+	
+	@OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Orders> restaurantOrders;
+	
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Orders> customerOrders;
 }
